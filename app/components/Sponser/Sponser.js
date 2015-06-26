57ee96755b5fb5c2eab451fc90a022aeb138096b
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2015-06-26 16:39:58 +0800

    Update sponsors' logo links

--- app/components/Sponser/Sponser.js
+++ app/components/Sponser/Sponser.js
@@ -5,21 +5,21 @@ module.exports = {
     "黃金級贊助" : [
       {
         "title" : "Appier",
-        "logo" : "//coscup.org/2015/assets//images/sponsor-appier.png",
+        "logo" : "http://coscup.org/2015/assets/images/sponsor-appier.png",
             "link" : "http://appier.com/"
       }
     ],
     "白銀級贊助" : [
     	{
     		"title" : "祐生研究基金會",
-    		"logo" : "//coscup.org/2015/assets//images/sponsor-archilife.jpg",
+    		"logo" : "http://coscup.org/2015/assets/images/sponsor-archilife.jpg",
             "link" : "http://www.archilife.org/"
     	}
     ],
     "青銅級贊助" : [
         {
         	"title" : "哇寶國際資訊",
-    		"logo" : "//coscup.org/2015/assets//images/sponsor-wabow.jpg",
+    		"logo" : "http://coscup.org/2015/assets/images/sponsor-wabow.jpg",
             "link" : "http://www.wabow.com/"
     	}
     ],
